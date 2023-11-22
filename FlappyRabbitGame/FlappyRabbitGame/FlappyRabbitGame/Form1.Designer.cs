4a7cb5b741ebce9c2ee463ec80e2ee153a3729c5
anniehayho <147356022+anniehayho@users.noreply.github.com>
2023-11-22 16:47:07 +0700

    Flappy Rabbit Game ver 1

---
+++ FlappyRabbitGame/FlappyRabbitGame/FlappyRabbitGame/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace FlappyRabbitGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.rabbit = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbScore = new System.Windows.Forms.Label();
+            this.pipeup = new System.Windows.Forms.PictureBox();
+            this.pipedown = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rabbit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.LightPink;
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(-3, 426);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(765, 114);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            // 
+            // rabbit
+            // 
+            this.rabbit.Image = ((System.Drawing.Image)(resources.GetObject("rabbit.Image")));
+            this.rabbit.Location = new System.Drawing.Point(96, 71);
+            this.rabbit.Name = "rabbit";
+            this.rabbit.Size = new System.Drawing.Size(70, 70);
+            this.rabbit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rabbit.TabIndex = 1;
+            this.rabbit.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.lbScore.Font = new System.Drawing.Font("Microsoft Tai Le", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbScore.ForeColor = System.Drawing.Color.HotPink;
+            this.lbScore.Location = new System.Drawing.Point(12, 482);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(95, 29);
+            this.lbScore.TabIndex = 2;
+            this.lbScore.Text = "Score: 0";
+            // 
+            // pipeup
+            // 
+            this.pipeup.Image = ((System.Drawing.Image)(resources.GetObject("pipeup.Image")));
+            this.pipeup.Location = new System.Drawing.Point(432, -19);
+            this.pipeup.Name = "pipeup";
+            this.pipeup.Size = new System.Drawing.Size(70, 160);
+            this.pipeup.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeup.TabIndex = 4;
+            this.pipeup.TabStop = false;
+            // 
+            // pipedown
+            // 
+            this.pipedown.Image = ((System.Drawing.Image)(resources.GetObject("pipedown.Image")));
+            this.pipedown.Location = new System.Drawing.Point(355, 270);
+            this.pipedown.Name = "pipedown";
+            this.pipedown.Size = new System.Drawing.Size(70, 160);
+            this.pipedown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipedown.TabIndex = 5;
+            this.pipedown.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(764, 575);
+            this.Controls.Add(this.pipeup);
+            this.Controls.Add(this.lbScore);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.rabbit);
+            this.Controls.Add(this.pipedown);
+            this.ForeColor = System.Drawing.Color.LightPink;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flappy Rabbit Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rabbit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox rabbit;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbScore;
+        private System.Windows.Forms.PictureBox pipeup;
+        private System.Windows.Forms.PictureBox pipedown;
+    }
+}
+
---
+++ FlappyRabbitGame/FlappyRabbitGame/FlappyRabbitGame/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyRabbitGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int gravity = 10;
+        int speed = 25;
+        int score = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 15;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -15;
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                timer1.Start();
+            }
+        }
+
+        Random rnd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rabbit.Top += gravity;
+            pipedown.Left -= speed;
+            pipeup.Left -= speed;
+            lbScore.Text = $"Score: {score}";
+            //adding score
+            if (pipedown.Left < -140)
+            {
+                pipedown.Left = rnd.Next(100, 600);
+                score++;
+            }
+            if (pipeup.Left < -140)
+            {
+                pipeup.Left = rnd.Next(300, 700);
+                score++;
+            }
+            if (rabbit.Bounds.IntersectsWith(pipedown.Bounds) || rabbit.Bounds.IntersectsWith(pipeup.Bounds) || rabbit.Bounds.IntersectsWith(ground.Bounds))
+            {
+                timer1.Stop();
+                lbScore.Text += " Game over!!!";
+            }
+            if (score > 10)
+            {
+                speed += 5;
+            }
+        }
+    }
+}
